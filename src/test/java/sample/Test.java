040b0078a99c1ce8ad2d68659dbac7dd0e1a0d23
yuvar <yuvar@BOOK-NVVFSQ1A0V>
2025-05-14 09:59:24 +0530

    First Commit

--- src/test/java/sample/Test.java
+++ src/test/java/sample/Test.java
@@ -5,5 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Facebook - Test");
+		System.out.println("Facebook - Test");
 	}
 }
